41ed79ebbfd2e33c4a80ab39b62190ae34b36ef5
Liang Chen Yu <yuliangchen24@gmail.com>
2026-04-25 00:53:57 +0100

    add conflict type constants

---
+++ backend/core/conflicts/types.py
@@ -0,0 +1,12 @@
+"""Conflict type constants."""
+
+TYPE_MISMATCH = "type_mismatch"
+NULLABLE_MISMATCH = "nullable_mismatch"
+LENGTH_MISMATCH = "length_mismatch"
+ENUM_MISMATCH = "enum_mismatch"
+FK_TARGET_MISMATCH = "fk_target_mismatch"
+DEFAULT_MISMATCH = "default_mismatch"
+
+INFO = "info"
+WARNING = "warning"
+ERROR = "error"
